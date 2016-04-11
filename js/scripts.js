f6a0cc0dd58acd0acb7b373ce446c183eb94f5f7
Noah Kittleson and Niem Nguyen <noah@>
2016-04-11 09:53:40 -0700

    added forms to page

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,19 @@
+function Pet (name, age, type) {
+  this.name = name;
+  this.age = age;
+  this.type = type;
+}
+
+
 $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
-    var petsInput = $("#pets").val();
+    var petsName = $("#petName").val();
+    var petsAge = parseInt($("#petAge").val());
+    var petsType = $("#petType").val();
+    alert(petsName + petsAge + petsType);
+
+    var newPet = new Pet (petsName, petsAge, petsType);
+
   });
 });
